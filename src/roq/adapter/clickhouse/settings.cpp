f5635017109a3685c073fa9f93500406f675bc54
thraneh <thraneh@gmail.com>
2023-07-03 16:30:09 +0200

    Use Settings (#356)

--- src/roq/adapter/clickhouse/settings.cpp
+++ src/roq/adapter/clickhouse/settings.cpp
@@ -6,6 +6,8 @@
 
 #include "roq/adapter.hpp"
 
+#include "roq/adapter/flags/settings.hpp"
+
 #include "roq/adapter/clickhouse/flags/flags.hpp"
 
 using namespace std::literals;
@@ -14,7 +16,10 @@ namespace roq {
 namespace adapter {
 namespace clickhouse {
 
-Settings::Settings() : adapter::Settings{adapter::create_settings(ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER)} {
+Settings::Settings()
+    : adapter::Settings{adapter::flags::create_settings(ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER)},
+      db_host{flags::Flags::db_host()}, db_port{flags::Flags::db_port()}, database{flags::Flags::database()},
+      max_rows{flags::Flags::max_rows()} {
   log::debug("settings={}"sv, *this);
 }
 
